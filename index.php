0235aff5fe88db74694b91fbc5fad77821d10546
Hanin Anthony <haninasi2014@gmail.com>
2020-01-13 09:35:58 +0100

    Création projet et ajout des 1eres cartes

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8"><!--codage caractère -->
+		<meta name="author" content="Anthony Hanin">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">		
+		<link href="style/style.css" rel="stylesheet">
+		<link rel="icon" href="">
+		<title></title>
+	</head>
+	<body>
+		<!-- 5 cartes,par pack
+		Règle 1 : 1 carte famille Simpson/Bouvier par tirage
+		Règle 2 : guest star proba 15%
+		Règle 3 : unique apparition 10% -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script type="text/javascript">
+			
+		</script>
+	</body>
+</html>
